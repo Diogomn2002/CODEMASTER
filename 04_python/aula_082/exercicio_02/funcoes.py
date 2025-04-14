d82b85e218632d2dbf40988ec6e809501a7454eb
Diogo Nascimento <diogo.morgado.nascimento@gmail.com>
2025-04-14 22:09:37 +0100

    atualizações

--- 04_python/aula_082/exercicio_02/funcoes.py
+++ 04_python/aula_082/exercicio_02/funcoes.py
@@ -2,7 +2,25 @@ import os
 import time
 
 # Funções
-
+def contador_numeros():
+  total_numeros = int(input("- Digite o total de números que serão analisados: "))
+  
+pares = 0
+impares = 0
+loop = 1
+  
+while loop <= total_numeros:
+  numero = int(input(f"- Digite o ({loop}º) número: "))
+      
+  if numero % 2 == 0:
+    pares += 1
+  else:
+    impares += 1
+      
+  loop += 1
+  
+print(f"\n--- Total de números PARES: ({pares}) ---")
+print(f"--- Total de números ÍMPARES: ({impares}) ---")
 
 
 # Funções Especiais
--- 04_python/aula_082/exercicio_02/main.py
+++ 04_python/aula_082/exercicio_02/main.py
@@ -4,17 +4,7 @@ limpa()
 
 print("\n==== Contabilizador de Números ====\n ")
 
-total_numeros = float(input("-Digite o total de números que serão analisados: "))
-contador = 1
-par = 0
-impar = 0
-
-while contador <= total_numeros:
-  idade = float(input(f"- Digite a idade o [{contador}] numero: "))
- 
-
-
-
+contador_numeros()
 
 print("\nFIM")
 
